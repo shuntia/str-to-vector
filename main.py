ad9177cda3df05e411932bb51852e883de63dbb0
Ethan Huang <ethan.yl.huang@gmail.com>
2025-04-29 09:23:00 -0700

    sklearn embeddings, add to pd dataframe

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+
+
 def main():
-    print("Hello from trigh-project!")
+    df = pd.DataFrame({"raw_texts": [], "embeddings": []})
+    text = input("Enter text to vectorize: ").splitlines()
+    vectorized = vectorize(text)
+    vectorized_df = pd.DataFrame(
+        {"raw_texts": text, "embeddings": [vec for vec in vectorized.toarray()]}
+    )
+    df = pd.concat([df, vectorized_df], axis=0, ignore_index=True)
+    print(df.head())
+
+
+def vectorize(text):
+    vectorizer = TfidfVectorizer()
+    vectorized = vectorizer.fit_transform(text)
+    return vectorized
 
 
 if __name__ == "__main__":
