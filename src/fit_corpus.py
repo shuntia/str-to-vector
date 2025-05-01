b6fcc64191630a160e244b649d3cbbd45cd264f2
shuntia <shunta@koga.us>
2025-05-01 09:29:29 -0700

    beautify HTML and add logging, imported argparse for future use.

--- src/fit_corpus.py
+++ src/fit_corpus.py
@@ -3,10 +3,16 @@ from vector_pipeline import VectorPipeline
 import joblib
 from datasets import load_dataset
 
-# corpus = fetch_20newsgroups(remove=("headers", "footers", "quotes")).data
-imdb_dataset = load_dataset("imdb")
-corpus = imdb_dataset["train"]["text"] + imdb_dataset["test"]["text"]
 
-pipe = VectorPipeline(n_components=3)
-pipe.fit(corpus)
-joblib.dump(pipe, "umap_prefit2.pkl")
+def load():
+    # corpus = fetch_20newsgroups(remove=("headers", "footers", "quotes")).data
+    imdb_dataset = load_dataset("imdb")
+    corpus = imdb_dataset["train"]["text"] + imdb_dataset["test"]["text"]
+
+    pipe = VectorPipeline(n_components=3)
+    pipe.fit(corpus)
+    joblib.dump(pipe, "umap_prefit2.pkl")
+
+
+if __name__ == "__main__":
+    load()
